46ac1c023e401ac13b5c8488bf9fa1465e99d54d
GrSkiy <43372592+GrSkiy@users.noreply.github.com>
2020-12-21 21:54:18 +0300

    Last branch commit

---
+++ src/components/ExchangeCardCurrent.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default class ExchangeCardCurrent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let { minorName, educationYear, credits, minorAdress } = this.props;
+
+    return (
+      <View>
+        <Text>Текущий майнор</Text>
+        <Text>{minorName}</Text>
+        <Text>{educationYear}</Text>
+        <Text>{credits}</Text>
+        <Text>{minorAdress}</Text>
+      </View>
+    );
+  }
+}
