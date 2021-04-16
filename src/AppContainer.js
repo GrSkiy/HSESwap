6c3f43e84d1e274216c576441052c7394d17c534
ALeo75C <za.iz.pr@yandex.kz>
2021-04-16 15:41:12 +0300

    bug fix

---
+++ src/AppContainer.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+import DB from './db'
+
+import {
+  linkFromTokens,
+  linkFromExchangeMinors,
+  fetchData
+} from './store/actions/api'
+
+import { getToken, updateToken } from './store/actions/tokens'
+import { getFilters } from './store/actions/filters'
+import { updateExchangeMinors } from './store/actions/exchangeMinors'
+
+import { UserNavigation, GuestNavigation } from './AppNavigation'
+import { PreloaderScreen } from './screens/PreloaderScreen'
+import MainForGuestScreen from './screens/MainForGuestScreen'
+
+function select(state) {
+  return {
+    tokens: state.tokens,
+    exchangeMinors: state.exchangeMinors.entities,
+    data_from_api: state.data_from_api
+    // url: state.data_from_api.url,
+    // pageData: state.data_from_api.pageData
+  }
+}
+
+class AppContainer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loaded: false
+    }
+  }
+
+  componentDidMount() {
+    const token = DB.getToken((result) => {
+      console.log('Redux Action getToken', result)
+      this.props.getToken(result)
+    })
+  }
+
+  componentDidUpdate() {
+    const { deviceToken, loaded } = this.props.tokens
+    const { exchangeMinors } = this.props
+    // const url = this.props.data_from_api.url
+    // let { pageData } = this.props.data_from_api
+
+    console.log('AppContainer PROPS', this.props)
+    console.log(
+      'TOKEN AND LOAD STATE',
+      deviceToken === '',
+      loaded === this.state.loaded
+    )
+
+    if (deviceToken === '' && loaded === false) {
+      console.log('FIRST IF')
+    } else if (deviceToken === '' && loaded === true) {
+      console.log('SECOND IF')
+
+      this.props.linkFromTokens()
+      if (this.props.data_from_api.url !== '') {
+        fetch(this.props.data_from_api.url)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log('Response from the server', data)
+
+            this.props.updateToken(data.tokens)
+            this.props.getFilters(data.filters)
+          })
+      }
+    } else if (deviceToken != '' && this.state.loaded === false) {
+      this.props.linkFromExchangeMinors(deviceToken)
+      if (this.props.data_from_api.url !== '') {
+        this.props.fetchData(
+          this.props.data_from_api.url,
+          this.props.updateExchangeMinors
+        )
+        this.setState({ loaded: true })
+      }
+    }
+  }
+
+  render() {
+    console.log('AppContainer', this.props)
+    // const login = false
+    const login = true
+    const { deviceToken } = this.props.tokens
+    return deviceToken === '' ? (
+      <PreloaderScreen />
+    ) : login ? (
+      <UserNavigation />
+    ) : (
+      <GuestNavigation />
+    )
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      linkFromTokens,
+      linkFromExchangeMinors,
+      getToken,
+      updateToken,
+      getFilters,
+      fetchData,
+      updateExchangeMinors
+    },
+    dispatch
+  )
+}
+
+export default connect(select, mapDispatchToProps)(AppContainer)
---
+++ src/AppNavigation.js
@@ -0,0 +1,303 @@
+import React from 'react'
+import { createAppContainer, ThemeColors } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
+import { createDrawerNavigator } from 'react-navigation-drawer'
+import { Platform, Text } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+
+import AppContainer from './AppContainer'
+
+import MainScreen from './screens/MainScreen'
+import MainForGuestScreen from './screens/MainForGuestScreen'
+import ExshangesForUserScreen from './screens/ExshangesForUserScreen'
+
+import LogInScreen from './screens/LogInScreen'
+import EmailVerificationScreen from './screens/EmailVerificationScreen'
+import BaseInfoSkreen from './screens/REGBaseInfoSkreen'
+import YouMinorSkreen from './screens/REGYouMinorSkreen'
+import YourWishedMinorsSkreen from './screens/REGWhishedMinorsSkreen'
+import PublishingSkreen from './screens/REGPublishedSkreen'
+
+// import FiltersScreen from './screens/FiltersScreen'
+import UsersExchangesScreens from './screens/UsersExchangesScreens'
+import ChatScreen from './screens/ChatScreen'
+
+import Burger from './screens/Burger'
+import Setting_view from './screens/Setting_view'
+import Setting_change from './screens/Setting_change'
+import AllMinorsScreen from './screens/AllMinorsScreen'
+
+import MinorScreen from './screens/MinorScreen'
+import ExchangeDescriptionScreen from './screens/ExchangeDescriptionScreen'
+import SuccessExchangeScreen from './screens/SuccessExchangeScreen'
+import SucsessExchangeDescriptionScreen from './screens/SucsessExchangeDescriptionScreen'
+import InformationOfExchangeScreen from './screens/InformationOfExchangeScreen'
+
+const navigatorOptions = {
+  defaultNavigationOptions: {
+    headerTitleAlign: 'center',
+    headerStyle: {
+      backgroundColor: '#FAFAFA'
+    },
+    headerTitleStyle: {
+      fontSize: 16,
+      fontWeight: '700'
+    }
+  }
+}
+
+const MainForUserStack = createStackNavigator(
+  {
+    Main: {
+      screen: MainScreen
+    }
+  },
+  navigatorOptions
+)
+
+const MainForGuestStack = createStackNavigator(
+  {
+    MainForGuest: {
+      screen: MainForGuestScreen
+    }
+  },
+  navigatorOptions
+)
+
+const LogInStack = createStackNavigator(
+  {
+    login: {
+      screen: LogInScreen
+    },
+    EmailVerification: {
+      screen: EmailVerificationScreen
+    },
+    Base: {
+      screen: BaseInfoSkreen
+    },
+    YourMinor: {
+      screen: YouMinorSkreen
+    },
+    Whished: {
+      screen: YourWishedMinorsSkreen
+    },
+    Publishing: {
+      screen: PublishingSkreen
+    }
+  },
+  navigatorOptions
+)
+
+const SettinsStack = createStackNavigator(
+  {
+    PersonData: {
+      screen: Setting_view
+    },
+    EditPersonData: {
+      screen: Setting_change
+    }
+  },
+  navigatorOptions
+)
+
+const UsersExchangesStack = createStackNavigator(
+  {
+    UsersExchanges: {
+      screen: UsersExchangesScreens
+    },
+    Chat: {
+      screen: ChatScreen
+    }
+  },
+  navigatorOptions
+  // { bottomTabsMode: 'none' }
+)
+
+const AllMinorsStack = createStackNavigator(
+  {
+    AllMinors: {
+      screen: AllMinorsScreen
+    }
+  },
+  navigatorOptions
+)
+
+const ForUserStack = createStackNavigator(
+  {
+    ForUserMain: {
+      screen: MainScreen
+    }
+  },
+  navigatorOptions
+)
+
+const ExchangeCardStack = createStackNavigator(
+  {
+    ExchangeCard: {
+      screen: ExchangeDescriptionScreen
+    }
+  },
+  navigatorOptions
+)
+
+const AllExchangesStack = createStackNavigator(
+  {
+    Main: {
+      screen: MainForUserStack
+    },
+    Chat: {
+      screen: UsersExchangesStack
+    },
+    Profile: {
+      screen: Burger
+    },
+    Settings: {
+      screen: SettinsStack
+    },
+    AllMinors: {
+      screen: AllMinorsStack
+    },
+    ExchangeCard: {
+      screen: ExchangeCardStack
+    },
+    SuccessExchange: {
+      screen: SuccessExchangeScreen
+    },
+    SucsessExchangeDescription: {
+      screen: SucsessExchangeDescriptionScreen
+    },
+    Information: {
+      screen: InformationOfExchangeScreen
+    },
+    Minor: {
+      screen: MinorScreen
+    },
+    login: {
+      screen: LogInStack
+    }
+  },
+  { headerMode: 'none' }
+)
+
+const ForUserMainStack = createStackNavigator(
+  {
+    ForUser: {
+      screen: MainForUserStack
+    },
+    Chat: {
+      screen: UsersExchangesStack
+    },
+    Profile: {
+      screen: Burger
+    },
+    Settings: {
+      screen: SettinsStack
+    },
+    ExchangeCard: {
+      screen: ExchangeCardStack
+    },
+    SuccessExchange: {
+      screen: SuccessExchangeScreen
+    },
+    SucsessExchangeDescription: {
+      screen: SucsessExchangeDescriptionScreen
+    },
+    Information: {
+      screen: InformationOfExchangeScreen
+    },
+    Minor: {
+      screen: MinorScreen
+    },
+    login: {
+      screen: LogInStack
+    }
+  },
+  { headerMode: 'none' }
+)
+
+const bottomTabsConfig = {
+  Main: {
+    screen: AllExchangesStack,
+    navigationOptions: {
+      tabBarLabel: 'Объявления',
+      tabBarIcon: (info) => (
+        <Ionicons name="grid" size={25} color={info.tintColor} />
+      )
+    }
+  },
+  ForUser: {
+    screen: ForUserMainStack,
+    navigationOptions: {
+      tabBarLabel: 'Для тебя',
+      tabBarIcon: (info) => (
+        <Ionicons name="home" size={25} color={info.tintColor} />
+      )
+    }
+  }
+}
+
+const BottomNavigator = createBottomTabNavigator(bottomTabsConfig, {
+  activeTintColor: '#005aab',
+  initialRouteName: 'ForUser',
+  shifting: true,
+  barStyle: {
+    backgroundColor: '#000'
+  }
+})
+
+const UserMainStack = createStackNavigator(
+  {
+    // Tabs: {
+    //   screen: BottomNavigator,
+    //   tabBarVisible: false,
+    //
+    //   swipeEnabled: true
+    // }
+    AllExchanges: {
+      screen: AllExchangesStack
+    },
+    ForUser: {
+      screen: ForUserStack
+    }
+  },
+  { headerMode: null }
+)
+
+const GuestMainStack = createStackNavigator(
+  {
+    GuestMain: {
+      screen: MainForGuestStack
+    },
+    ExchangeCard: {
+      screen: ExchangeCardStack
+    },
+    Login: {
+      screen: LogInStack
+    }
+    // Container: {
+    //   screen: AppContainerStack
+    // }
+  },
+  { headerMode: null }
+)
+
+// const AppNavigator = createStackNavigator(
+//   {
+//     UserPart: {
+//       screen: UserMainStack
+//     },
+//     GuestPart: {
+//       screen: GuestMainStack
+//     }
+//   },
+//   { headerMode: null }
+// )
+// const AppNavigation = createAppContainer(AppNavigator)
+
+const UserNavigation = createAppContainer(UserMainStack)
+const GuestNavigation = createAppContainer(GuestMainStack)
+
+// export { AppNavigation }
+export { UserNavigation, GuestNavigation }
